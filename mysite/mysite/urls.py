fac806272b78eb4d28208efbf06b9225913deec9
Tadas <tadasmasnauskas@gmail.com>
2023-04-18 21:27:09 +0300

    work on layout

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,12 +22,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('travel_diary.urls')),
-  
-    
-]
+     
+] +(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
     
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
